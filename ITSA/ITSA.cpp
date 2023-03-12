69463a822d5f2f291faba9b32d9b4dfa6eb32262
Xiangxiang99141 <roys99141@gmail.com>
2023-03-12 23:28:56 +0800

    修正不知道為什麼多出來的東西

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -5,40 +5,31 @@
 using namespace std;
 int main()
 {
-#include <iostream>
-    using namespace std;
-    int main()
-    {
-        int a = 0;
-        cin >> a;
-        switch (a) {
-        case 12:
-        case 1:
-        case 2:
-            cout << "Winter\n";
-            break;
-        case 3:
-        case 4:
-        case 5:
-            cout << "Spring\n";
-            break;
-        case 6:
-        case 7:
-        case 8:
-            cout << "Summer\n";
-            break;
-        case 9:
-        case 10:
-        case 11:
-            cout << "Autumn\n";
-            break;
-        }
-
-
-        system("pause");
+    int a = 0;
+    cin >> a;
+    switch (a) {
+    case 12:
+    case 1:
+    case 2:
+        cout << "Winter\n";
+        break;
+    case 3:
+    case 4:
+    case 5:
+        cout << "Spring\n";
+        break;
+    case 6:
+    case 7:
+    case 8:
+        cout << "Summer\n";
+        break;
+    case 9:
+    case 10:
+    case 11:
+        cout << "Autumn\n";
+        break;
     }
 
-    
     system("pause");
 }
 
