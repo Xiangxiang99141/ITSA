1d2304b043fc314d1f5c32ba3f704ebec6a0ec97
Xiangxiang99141 <roys99141@gmail.com>
2023-03-13 09:31:53 +0800

    修正小東西

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -13,7 +13,7 @@ int main()
     cout << "這個圓的半徑是(直徑/2) : " << R/2 << endl;
     cout << "請輸入一個整數座標 : \n";*/
     while (cin >> x >> y) {
-        if ((pow(x, 2) + pow(y, 2) < pow(R / 2, 2))) {
+        if ((pow(x, 2) + pow(y, 2) <= pow(R / 2, 2))) {
             cout << "inside\n";
         }
         else {
