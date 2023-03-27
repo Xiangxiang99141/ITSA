dbee6fb27f785d4ab1f52a0cf634a589c0deff30
Xiangxiang99141 <roys99141@gmail.com>
2023-03-27 15:03:39 +0800

    這題小繞

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -3,10 +3,23 @@
 
 #include <iostream>
 using namespace std;
+int clacstrcount(string &s,int offset,string &l) {
+    int a = 0;
+    if (offset > l.length()) return a = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (l[i + offset] == s[i]) continue;
+        else return a += clacstrcount(s, offset + 1, l);
+    }
+    a++;
+    return a+=clacstrcount(s,offset+1,l);
+}
 int main()
 {
-    std::cout << "Hello World!\n";
-    system("pause");
+    string str_input, str_all;
+    cin >> str_input;
+    cin >> str_all;
+    cout << clacstrcount(str_input,0,str_all) << endl;
+    return 0;
 }
 
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
