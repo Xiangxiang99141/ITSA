a5fe2e4f93f81f4d842dd2b7788868ccc56393ef
Xiangxiang99141 <roys99141@gmail.com>
2023-06-30 16:24:18 +0800

    真的不知道為什麼ITSA說我錯

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -3,92 +3,96 @@
 
 #include <iostream>
 #include <string>
-#include <sstream>
 #include <vector>
 using namespace std;
-//排序
-//void cardsort(vector<string>& a, int n) {
-//	for (int i = 0; i < n; i++) {
-//		for (int j=0; j < n; j++) {
-//			if (a[i][0] > a[j][0]) {
-//				string t;
-//				t = a[i]; a[i] = a[j]; a[j] = t;
-//			}
-//		}
-//	}
-//}
-
-void cardsort(vector<string>& a, int n) {
-	vector<int> b;
-	for (int i = 0; i < n; i++) {
-		switch (a[i][0]) {
-		case 'S':
-			b.push_back(stoi(a[i].substr('S'))+400);
-		case 'H':
-			b.push_back(stoi(a[i].substr('H'))+300);
-		case 'D':
-			b.push_back(stoi(a[i].substr('D')) + 200);
-		case 'C ':
-			b.push_back(stoi(a[i].substr('D')) + 100);
-		}
-	}
-
-	for (int i = 0; i < b.size(); i++) {
-		for (int j=0; j < n; j++) {
-			if (b[i] > b[j]) {
-				int t;
-				t = b[i]; b[i] = b[j]; b[j] = t;
-			}
-		}
-	}
-	a.clear();
-	for (int i = 0; i < n; i++) {
-		a.push_back(b[i]);
-	}
-	
-}
+void cardsort(vector<string>&,int);
+void split2Vec(string, int, vector<string>&, string);
+void card2int(vector<string>&, vector<int>&);
+void intsort(vector<int>&);
 
 int main()
 {
-	int n = 0;
-	while (cin >> n) {
-		string str1,s;
-		vector<string> str2;
-		vector<string> all;
-		cin.ignore();
-		for (int i = 0; i < n; i++) {
-			getline(cin, str1);
-			istringstream x(str1);
-			while (getline(x, s, ' ')) {
-				str2.push_back(s);
-			}
-			cardsort(str2, str2.size());
-		//	string t;
-		//	for (int j = 0; j < str2.size();j++) {
-		//		if (i == str2.size() - 1) 
-		//		{
-		//			t += str2[j];
-		//		}
-		//		else{
-		//			t += str2[j] + " ";
-		//		}
-		//	}
-		//	str2.clear();
-		//	all.push_back(t);
-		//}
-		//for (int i = 0; i < all.size(); i++) {
-		//		cout << all[i] << endl;
-		//}
+	int n=0; //撲克牌疊數
+    cin >> n;
+    cin.ignore();
+	for (int i = 0; i < n; i++) {
+        string s;
+		vector<string> cardarray;
+        getline(cin, s);
+        split2Vec(s, 0, cardarray, " ");
+        cardsort(cardarray, n);
+        for (int i = 0; i < cardarray.size(); i++) {
+            cout << cardarray[i];
+            if (i < cardarray.size() - 1) cout << " ";
+            else cout << endl;
+        }
+		cardarray.clear();
 	}
+    return 0;
 }
 
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+void card2int(vector<string>& buf,vector<int>& ibuf) {
+    for (int i = 0; i < buf.size(); i++) {
+        int tmp = stoi(buf[i].substr(1,buf[i].length()));
+        switch (buf[i][0]) {
+        case 'S': 
+            ibuf.push_back(tmp + 400);
+            break;
+        case 'H':
+            ibuf.push_back(tmp + 300);
+            break;
+        case 'D':
+            ibuf.push_back(tmp + 200);
+            break;
+        case 'C':
+            ibuf.push_back(tmp + 100);
+            break;
+        }
+    }
+}
 
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
+
+//排序
+void intsort(vector<int>& buf) {
+    for (int i = 0; i < buf.size()-1; i++) {
+        for (int j = 0; j < buf.size()-i-1; j++) {
+            if (buf[j] < buf[j+1]) {
+                int t;
+                t = buf[j+1]; buf[j+1] = buf[j]; buf[j] = t;
+            }
+        }
+    }
+}
+void cardsort(vector<string>& cardarray, int n) {
+    vector<int> buf;
+    card2int(cardarray, buf);
+    intsort(buf);
+    cardarray.clear();
+    for (int i = 0; i < buf.size(); i++) {
+        if (buf[i] < 500 && buf[i]>400) {
+            cardarray.push_back("S" + to_string(buf[i] - 400));
+        }
+        else if (buf[i] < 400 && buf[i]>300) {
+            cardarray.push_back("H" + to_string(buf[i] - 300));
+        }
+        else if (buf[i] < 300 && buf[i]>200) {
+            cardarray.push_back("D" + to_string(buf[i] - 200));
+        }
+        else if (buf[i] < 200 && buf[i]>100) {
+            cardarray.push_back("C" + to_string(buf[i] - 100));
+        }
+    }
+}
+
+
+void split2Vec(string s, int offset, vector<string>& buf, string symbol) {
+    size_t next = 0;
+    do {
+        next = s.find_first_of(symbol, offset);
+        if (offset != next) {
+            string tmp = s.substr(offset, next - offset);
+            buf.push_back(tmp); // 空字串不儲存  
+        }
+        offset = next + 1;
+    } while (next != string::npos);
+}
